bfb4f885c81cb657c9c4ea8586e30ab6075afd3b
weslysantos <marcosweslyufc85@alu.ufc.br>
2021-04-10 18:07:25 -0300

    projeto cinema

---
+++ projeto_06_cinema/Cinema.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+class Cliente{
+    String id;
+    String fone;
+
+}
+
+class Cinema{
+    private ArrayList<Cliente> cliente = new ArrayList<>();
+
+
+
+    void reservar{
+        
+    }
+    void cancelar{
+
+    }
+
+}
+
+public class Sala {
+    
+    public static void main(String[] args) {
+    
+    
+    
+    }
+
+
+}
